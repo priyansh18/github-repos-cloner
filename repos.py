9eba1d6fdb54d7b359aafdd3375ed5dea04b9c7f
priyansh18 <singhalpriyansh58@gmail.com>
2021-05-11 19:11:31 +0530

    Public Repository Extracted

--- repos.py
+++ repos.py
@@ -1,17 +1,11 @@
 from bs4 import BeautifulSoup
 import requests
+import os
 
 username = 'priyansh18'
 url = "https://github.com/{}?tab=repositories".format(username)
 allLinks = []
 isLastPage = False
-
-def allRepos(soup):
-    all_repos = soup.find_all('h3',{'class':'wb-break-all'})
-    for repos in all_repos:
-        link = repos.find('a',{'itemprop':'name codeRepository'})
-        itemlink = "https://github.com" + link.get('href')
-        allLinks.append(itemlink)
 
 while True:
     if isLastPage:
@@ -19,7 +13,21 @@ while True:
     response = requests.get(url)
     data = response.content
     soup = BeautifulSoup(data,'html.parser')
-    allRepos(soup)
+    signup = soup.find('a',{'data-target':'nux-signup-candidates.signupAction'})
+    if signup.text == 'Sign up':
+        all_repos = soup.find_all('h3',{'class':'wb-break-all'})
+        for repos in all_repos:
+            link = repos.find('a',{'itemprop':'name codeRepository'})
+            itemlink = "https://github.com" + link.get('href')
+            allLinks.append(itemlink)
+    else:
+        all_repos = soup.find_all('div',{'class':'private source'})
+        for repos in all_repos:
+            link = repos.find('a',{'itemprop':'name codeRepository'})
+            itemlink = "https://github.com" + link.get('href')
+            allLinks.append(itemlink)
+
+        
     buttonDiv = soup.find_all('a',{'class':'BtnGroup-item'})
     for divs in buttonDiv:
         if (divs.text == 'Next'):
